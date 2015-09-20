7979b092b9a92553f120967a639f30e495404f9f
ander335 <AnderWen@yandex.ru>
2015-09-20 17:40:35 +0300

    Java LSD

--- code/LSD.java
+++ code/LSD.java
@@ -1,21 +1,56 @@
 import static Sorter.Sorter.SortJava;
-import static Sorter.Sorter.SortC;
+
+import java.util.Random;
 
 public class LSD {
+	private static final int arraySize = 100;
+	
+	static {
+		System.loadLibrary("Sorter_Sorter");
+	}
+	
+	private static void FillArray(int mass[]) {
+		Random rand = new Random();
+		for (int i = 0; i < mass.length; ++i) {
+			mass[i] = rand.nextInt(1000000000);
+		}
+	}
+	
+	private static void PrintArray(int mass[]) {
+		for (int val : mass) {
+			System.out.println(val);
+		}
+	}
+	
+	private static boolean isSorted(int mass[]) {
+		for (int i = 0; i < mass.length - 1; ++i) {
+			if (mass[i] > mass[i + 1]) {
+				return false;
+			}
+		}
+		return true;
+	}
 	
 	public static void main(String[] args) {
-		System.out.println("Helloy World!");
+		int[] mass = new int[arraySize];
 		
-		int[] mass = new int[10];
-		
-		SortJava(mass);
-		System.out.println(mass[0]);
-		SortC(mass);
-		System.out.println(mass[0]);
-		
-		long time = System.nanoTime();
-		SortC(mass);
-		System.out.println((System.nanoTime() - time) / 1000000.);
+		for (int i = 0; i < 1000000000; ++i) {
+			FillArray(mass);
+			
+			SortJava(mass);
+			
+			if (isSorted(mass)) {
+				if (i % 100000 == 0) {
+					System.out.println(String.format("test number - %4d was pass!\n", i));
+				}
+			} else {
+				PrintArray(mass);
+				System.out.println("fail!");
+				System.exit(-1);
+			}
+		}
+		System.exit(0);
+
 	}
 
 }
--- code/Sorter/Sorter.java
+++ code/Sorter/Sorter.java
@@ -1,13 +1,44 @@
 package Sorter;
 
+import java.util.Arrays;
+
 public final class Sorter {
-	static {
-		System.loadLibrary("Sorter_Sorter");
-	}
 	private Sorter() {};
 	
 	public static native void SortC(int[] mass);
-	public static void SortJava(int[] mass) { 
-		mass[0] = 104362;
+	
+	public static void SortJava(int[] mass) {
+		final int maxOrder = 9;
+		int[] chars = new int[10];
+		int[] position = new int[10];
+		int[] tmpMass = new int[mass.length];
+		
+		for (int order = 0; order <= maxOrder; ++order) {
+			Arrays.fill(chars, 0);
+			for (int val : mass) {
+				chars[GetChar(val, order)]++;
+			}
+			
+			position[0] = 0;
+			for (int i = 1; i < 10; ++i) {
+				position[i] = chars[i - 1] + position[i - 1];
+			}
+			
+			for (int val : mass) {
+				tmpMass[position[GetChar(val, order)]++] = val;
+			}
+			
+			System.arraycopy(tmpMass, 0, mass, 0, mass.length);
+		}
+	}
+	
+	private static int GetChar(int val, int idx) {
+		int divider = 1;
+		
+		while (idx-- > 0) {
+			divider *= 10;
+		}
+		
+		return (val / divider) % 10;
 	}
 }
